459407dfb125fd9b038f6071a0eafee5a02b3a91
snakeyes1637@yandex.ru <snakeyes1637@yandex.ru>
2020-07-01 17:22:52 +0300

    Добавьте файлы проекта.

---
+++ laba 2.3/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace laba_2._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxZapyat = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.TextCelsius = new System.Windows.Forms.TextBox();
+            this.TextKelvin = new System.Windows.Forms.TextBox();
+            this.TextFahrenheit = new System.Windows.Forms.TextBox();
+            this.TextReaumur = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBoxZapyat
+            // 
+            this.comboBoxZapyat.FormattingEnabled = true;
+            this.comboBoxZapyat.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8"});
+            this.comboBoxZapyat.Location = new System.Drawing.Point(159, 318);
+            this.comboBoxZapyat.Name = "comboBoxZapyat";
+            this.comboBoxZapyat.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxZapyat.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "точка",
+            "запятая"});
+            this.comboBox2.Location = new System.Drawing.Point(376, 317);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Да",
+            "Нет"});
+            this.comboBox3.Location = new System.Drawing.Point(586, 316);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 2;
+            // 
+            // TextCelsius
+            // 
+            this.TextCelsius.Location = new System.Drawing.Point(171, 102);
+            this.TextCelsius.Name = "TextCelsius";
+            this.TextCelsius.Size = new System.Drawing.Size(100, 20);
+            this.TextCelsius.TabIndex = 3;
+            this.TextCelsius.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextCelsius_KeyPress);
+            this.TextCelsius.Leave += new System.EventHandler(this.TextCelsius_Leave);
+            // 
+            // TextKelvin
+            // 
+            this.TextKelvin.Location = new System.Drawing.Point(171, 214);
+            this.TextKelvin.Name = "TextKelvin";
+            this.TextKelvin.Size = new System.Drawing.Size(100, 20);
+            this.TextKelvin.TabIndex = 4;
+            this.TextKelvin.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextKelvin_KeyPress);
+            this.TextKelvin.Leave += new System.EventHandler(this.TextKelvin_Leave);
+            // 
+            // TextFahrenheit
+            // 
+            this.TextFahrenheit.Location = new System.Drawing.Point(467, 101);
+            this.TextFahrenheit.Name = "TextFahrenheit";
+            this.TextFahrenheit.Size = new System.Drawing.Size(100, 20);
+            this.TextFahrenheit.TabIndex = 5;
+            this.TextFahrenheit.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextFahrenheit_KeyPress);
+            this.TextFahrenheit.Leave += new System.EventHandler(this.TextFahrenheit_Leave);
+            // 
+            // TextReaumur
+            // 
+            this.TextReaumur.Location = new System.Drawing.Point(467, 214);
+            this.TextReaumur.Name = "TextReaumur";
+            this.TextReaumur.Size = new System.Drawing.Size(100, 20);
+            this.TextReaumur.TabIndex = 6;
+            this.TextReaumur.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextReaumur_KeyPress);
+            this.TextReaumur.Leave += new System.EventHandler(this.TextReaumur_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Цельсий";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(376, 217);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Реамюр";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(77, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Кельвин";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(360, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Фаренгейт";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 324);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "знаки после запятой";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextReaumur);
+            this.Controls.Add(this.TextFahrenheit);
+            this.Controls.Add(this.TextKelvin);
+            this.Controls.Add(this.TextCelsius);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBoxZapyat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxZapyat;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.TextBox TextCelsius;
+        private System.Windows.Forms.TextBox TextKelvin;
+        private System.Windows.Forms.TextBox TextFahrenheit;
+        private System.Windows.Forms.TextBox TextReaumur;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ laba 2.3/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace laba_2._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public double Mentol(double chto)
+        {
+            if (comboBoxZapyat.SelectedItem == null)
+            {
+                int stri = 0;
+                int y = Convert.ToInt32(stri);
+                double min = Math.Round(chto, y);
+                return min;
+            }
+            else
+            {
+                string selectedState = comboBoxZapyat.SelectedItem.ToString();
+                int y = Convert.ToInt32(selectedState);
+                double min = Math.Round(chto, y);
+                return min;
+            }
+        }
+
+
+ 
+
+        private void TextReaumur_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47||e.KeyChar >= 58) && number != 8 && (e.KeyChar <= 39||e.KeyChar >= 46) && number != 47 && number != 61) 
+                {
+                e.Handled = true;
+            }
+        }
+
+        private void TextCelsius_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47||e.KeyChar >= 58) && number != 8 && (e.KeyChar <= 39||e.KeyChar >= 46) && number != 47 && number != 61)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TextKelvin_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47  ||e.KeyChar >= 58) && number != 8 && (e.KeyChar <= 39||e.KeyChar >= 46) && number != 47 && number != 61)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TextFahrenheit_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47||e.KeyChar >= 58) && number != 8 && (e.KeyChar <= 39||e.KeyChar >= 46) && number != 47 && number != 61)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TextCelsius_Leave(object sender, EventArgs e)
+        {
+            String s, s1;
+            s = TextCelsius.Text;
+
+            if (!string.IsNullOrEmpty(TextCelsius.Text))
+            {
+                s1 = s.Substring(TextCelsius.Text.Length - 1, 1);
+
+                if (s1 == ",")
+                {
+
+                }
+                if (s1 == "-")
+                {
+
+                }
+
+                else if ((s1 == "0") | (s1 == "1") | (s1 == "2") | (s1 == "3") | (s1 == "4") | (s1 == "5") | (s1 == "6") | (s1 == "7") | (s1 == "8") | (s1 == "9"))
+                {
+
+
+                    new Thread(() =>
+                    {
+                        BeginInvoke((MethodInvoker)(() =>
+                        {
+
+
+
+                            double nutre = Convert.ToDouble(TextCelsius.Text);
+                            TextCelsius.Text = nutre.ToString();
+
+                            double kelvin = nutre - 273.15;
+                            double kusok = Mentol(kelvin);
+                            TextKelvin.Text = kusok.ToString();
+
+                            double farengeyt = (nutre * 9 / 5) + 32;
+                            double chel = Mentol(farengeyt);
+                            TextFahrenheit.Text = chel.ToString();
+
+                            double rumka = nutre * 8 / 10;
+                            double nine = Mentol(rumka);
+                            TextReaumur.Text = nine.ToString();
+
+                        }));
+
+
+                    }).Start();
+                }
+            }
+        }
+
+        private void TextFahrenheit_Leave(object sender, EventArgs e)
+        {
+
+            String s, s1;
+            s = TextFahrenheit.Text;
+
+            if (!string.IsNullOrEmpty(TextFahrenheit.Text))
+            {
+                s1 = s.Substring(TextFahrenheit.Text.Length - 1, 1);
+
+                if (s1 == ",")
+                {
+
+                }
+                if (s1 == "-")
+                {
+
+                }
+
+                else if ((s1 == "0") | (s1 == "1") | (s1 == "2") | (s1 == "3") | (s1 == "4") | (s1 == "5") | (s1 == "6") | (s1 == "7") | (s1 == "8") | (s1 == "9"))
+                {
+                    new Thread(() =>
+                    {
+                        BeginInvoke((MethodInvoker)(() =>
+                        {
+                            var might = Convert.ToDouble(TextFahrenheit.Text);
+                            TextFahrenheit.Text = might.ToString();
+
+                            double celsius = (might - 32) * 5 / 9;
+                            double nemo = Mentol(celsius);
+                            TextCelsius.Text = nemo.ToString();
+
+                            double kelvin = (might - 32) * 5 / 9 + 273.15;
+                            double kapitan = Mentol(kelvin);
+                            TextKelvin.Text = kapitan.ToString();
+
+                            double rumka = (might - 32) * 4 / 9;
+                            double miron = Mentol(rumka);
+                            TextReaumur.Text = miron.ToString();
+
+                        }));
+
+
+                    }).Start();
+                }
+            }
+        }
+
+        private void TextKelvin_Leave(object sender, EventArgs e)
+        {
+
+
+            String s, s1;
+            s = TextKelvin.Text;
+
+            if (!string.IsNullOrEmpty(TextKelvin.Text))
+            {
+                s1 = s.Substring(TextKelvin.Text.Length - 1, 1);
+
+                if (s1 == ",")
+                {
+
+                }
+                if (s1 == "-")
+                {
+
+                }
+
+                else if ((s1 == "0") | (s1 == "1") | (s1 == "2") | (s1 == "3") | (s1 == "4") | (s1 == "5") | (s1 == "6") | (s1 == "7") | (s1 == "8") | (s1 == "9"))
+                {
+                    new Thread(() =>
+                    {
+                        BeginInvoke((MethodInvoker)(() =>
+                        {
+
+
+                            var night = Convert.ToDouble(TextKelvin.Text);
+                            TextKelvin.Text = night.ToString();
+
+                            double celsius = night + 273.15;
+                            double kazaxi = Mentol(celsius);
+                            TextCelsius.Text = kazaxi.ToString();
+
+                            double farengeyt = (night - 273.15) * 9 / 5 + 32;
+                            double nositeli = Mentol(farengeyt);
+                            TextFahrenheit.Text = nositeli.ToString();
+
+                            double rumka = (night - 273.15) * 0.8;
+                            double koron = Mentol(rumka);
+                            TextReaumur.Text = koron.ToString();
+
+                        }));
+
+
+                    }).Start();
+                }
+            }
+        }
+
+        private void TextReaumur_Leave(object sender, EventArgs e)
+        {
+            String s, s1;
+            s = TextReaumur.Text;
+
+            if (!string.IsNullOrEmpty(TextReaumur.Text))
+            {
+                s1 = s.Substring(TextReaumur.Text.Length - 1, 1);
+
+                if (s1 == ",")
+                {
+
+                }
+                if (s1 == "-")
+                {
+
+                }
+
+                else if ((s1 == "0") | (s1 == "1") | (s1 == "2") | (s1 == "3") | (s1 == "4") | (s1 == "5") | (s1 == "6") | (s1 == "7") | (s1 == "8") | (s1 == "9"))
+                {
+                    new Thread(() =>
+                    {
+                        BeginInvoke((MethodInvoker)(() =>
+                        {
+                            var bight = Convert.ToDouble(TextReaumur.Text);
+                            TextReaumur.Text = bight.ToString();
+
+                            double celsius = bight * 5 / 4;
+                            double tite = Mentol(celsius);
+                            TextCelsius.Text = tite.ToString();
+
+                            double kelvin = (bight * (5 / 4)) + 273.15;
+                            double meme = Mentol(kelvin);
+                            TextKelvin.Text = meme.ToString();
+
+                            double farengeyt = (bight * (9 / 4) + 32);
+                            double nene = Mentol(farengeyt);
+                            TextFahrenheit.Text = nene.ToString();
+
+                        }));
+
+
+                    }).Start();
+
+
+                }
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
+    
+
+
